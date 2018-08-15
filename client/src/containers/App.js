d622c784b89cca41c946c07eb8d8bf44fd685e6b
avshalomp <avshi1987@gmail.com>
2018-08-15 17:15:29 +1200

    client - add navbar component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux'; //so we can pass the store object as a prop
 import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom'; //so we can navigate from page to page
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = () => (
     <Provider store={store}>
         <Router>
-            <div>
-                Hello World!
+            <div className="onboarding">
+                <Navbar />
             </div>
         </Router>
     </Provider>
