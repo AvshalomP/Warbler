e54c7abf8ea0a1749a8b8a9c588ca3d1e0377e8d
avshalomp <avshi1987@gmail.com>
2018-08-15 16:06:53 +1200

    client - add redux store

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-//import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux'; //so we can pass the store object as a prop
+import { configureStore } from '../store';
+import { BrowserRouter as Router } from 'react-router-dom'; //so we can navigate from page to page
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const store = configureStore();
 
-      </div>
-    );
-  }
-}
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <div>
+                Hello World!
+            </div>
+        </Router>
+    </Provider>
+);
 
 export default App;
