69cac80defda012c03c30cd4635658413742428c
avshalomp <avshi1987@gmail.com>
2018-08-15 13:49:18 +1200

    initial client commit

---
+++ client/src/store/reducers/currentUser.js
@@ -0,0 +1,20 @@
+import { SET_CURRENT_USER } from "../actionTypes";
+
+const DEFAULT_STATE = {
+    isAuthenticated: false, // hopefully will be true, when logged in
+    user: {} // will contain all the user info when logged in
+};
+
+
+export default (state = DEFAULT_STATE, action) => {
+    switch (action.type){
+        case SET_CURRENT_USER:
+            return {
+                // if the are keys in action.user object make true else false
+                isAuthenticated: Object.keys(action.user).length > 0,
+                user: action.user
+            };
+        default:
+            return state;
+    }
+}
